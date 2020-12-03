457b28c9805e4c3ccf8c5b262eb89ea7b3ee34ba
Rodrigo Barreto <rodrigo.barreto@qi.edu.br>
2020-12-03 19:16:07 -0300

    Exercicio das caixas

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<!--Tags básicas do head-->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cálculos de Caixas de Papelão</title>
+	<!--Link dos nossos arquivos CSS e JS padrão-->
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<script src="js/scripts.js"></script>
+	<!--Link dos arquivos CSS e JS do Bootstrap-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+	<div class="container">
+		<div class="row text-center" id="titulo">
+			<div class="col-md-12">
+				<h1>Construção de caixas de água</h1>
+			</div>
+		</div>
+
+		<div class="row text-center">
+			<div class="col-md-6 coluna">
+				<h2>DADOS DA CAIXA DESEJADA</h2>
+				
+				<div class="row">
+					<div class="col-md-6">
+						<h3>Caixa retangular</h3>
+						<img src="img/retangular.jpg" class="imagem">
+						<!--Criar o form pedindo os valores da caixa retangular-->
+						<form>
+					
+						</form>
+					</div>
+					<div class="col-md-6">
+					<h3>Caixa cilíndrica</h3>
+					<img src="img/cilindrica.jpg" class="imagem">
+						<!--Criar o form pedindo os valores da caixa cilíndrica-->
+						<form>
+				
+						</form>
+					</div>
+
+				</div>
+				
+			</div>
+
+			<div class="col-md-6 coluna">
+				<h2>RESULTADOS</h2>
+				<!--mostrar todos os resultados aqui um abaixo do outro-->
+				<!--abrir aqui o PHP e testar qual o valor do botão recebido-->
+				<!--assim sabemos quais cálculos fazer (da retangular ou cilíndrica)-->
+
+			</div>
+		</div>
+
+	
+	</div>
+
+	
+</body>
+
+</html>
